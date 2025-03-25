ecfaba6679069dff3e57b6e7ca89df018a78f5fa
laplace-anon <150580554+laplace-anon@users.noreply.github.com>
2025-03-25 01:41:27 -0700

    chore: update benchmarks to use tinybench 4.0.1, adjust fetch tests, and add build script for server-express

--- benchmarks/index.js
+++ benchmarks/index.js
@@ -5,15 +5,18 @@ const bench = new Bench({ time: 10000 })
 const param = '/get/your_token_here'
 
 bench
+  // bun run dev
   .add('bun-hono', async () => {
     await fetch(`http://localhost:8088${param}`)
   })
+  // bun run index.ts
   .add('bun-express', async () => {
     await fetch(`http://localhost:8089${param}`)
   })
-  .add('node-express', async () => {
-    await fetch(`http://localhost:8090${param}`)
-  })
+// PORT=8090 npm run build
+// .add('node-express', async () => {
+//   await fetch(`http://localhost:8090${param}`)
+// })
 
 await bench.run()
 
